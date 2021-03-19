e66563fc2959790f34d0fd07be83710f627cbed3
Gaurav Mishra <grvmishra788@gmail.com>
2021-03-19 11:53:16 +0530

    Added code to animate moves

--- Constants.py
+++ Constants.py
@@ -5,3 +5,4 @@ MAX_FPS = 15
 FOUR_WAY_DIRS = [(-1, 0), (0, 1), (1, 0), (0, -1)]  # UP, RIGHT, DOWN, LEFT
 DIAGONAL_DIRS = [(-1, -1), (-1, 1), (1, 1), (1, -1)]  # TOP_LEFT, TOP_RIGHT, BOTTOM_RIGHT, BOTTOM_LEFT
 KNIGHT_DIRS = [(-1, -2), (-2, -1), (-2, 1), (-1, 2), (1, -2), (2, -1), (2, 1), (1, 2)]
+COLORS = ["white", "gray"]
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame as p
 import ChessEngine
-from Constants import WIDTH, HEIGHT, DIMS, SQ_SIZE, MAX_FPS
+from Constants import WIDTH, HEIGHT, DIMS, SQ_SIZE, MAX_FPS, COLORS
 
 IMAGES = {}
 
@@ -12,7 +12,6 @@ def load_images():
 
 
 def highlight_squares(screen, currState, validMoves, squareSelected):
-    colors = [p.Color("white"), p.Color("gray")]
     if squareSelected != ():
         r, c = squareSelected
         if currState.board[r][c][0] == ('w' if currState.whiteToMove else 'b'):
@@ -24,7 +23,7 @@ def highlight_squares(screen, currState, validMoves, squareSelected):
             s1.set_alpha(100)
             for move in validMoves:
                 if move.startRow == r and move.startCol == c:
-                    color = colors[(move.endRow + move.endCol) % 2]
+                    color = p.Color(COLORS[(r + c) % 2])
                     if currState.board[move.endRow][move.endCol] == "--":
                         s1.fill(color)
                         p.draw.circle(s1, p.Color("blue"), (SQ_SIZE // 2, SQ_SIZE // 2), 10)
@@ -34,6 +33,28 @@ def highlight_squares(screen, currState, validMoves, squareSelected):
                     screen.blit(s1, (move.endCol * SQ_SIZE, move.endRow * SQ_SIZE))
 
 
+def animate_move(move, screen, board, clock):
+    dR = move.endRow - move.startRow
+    dC = move.endCol - move.startCol
+    framesPerSecond = 10
+    frameCount = (abs(dR) + abs(dC)) * framesPerSecond
+    for frame in range(frameCount + 1):
+        r, c = (move.startRow + dR * frame / frameCount, move.startCol + dC * frame / frameCount)
+        draw_board(screen)
+        draw_pieces(screen, board)
+        # erase the piece moved from it's ending square
+        color = p.Color(COLORS[(move.endRow + move.endCol) % 2])
+        endSquare = p.Rect(move.endCol * SQ_SIZE, move.endRow * SQ_SIZE, SQ_SIZE, SQ_SIZE)
+        p.draw.rect(screen, color, endSquare)
+        # draw captured piece onto the rectangle
+        if move.pieceCaptured != "--":
+            screen.blit(IMAGES[move.pieceCaptured], endSquare)
+        # draw moving piece
+        screen.blit(IMAGES[move.pieceMoved], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+        p.display.flip()
+        clock.tick(60)
+
+
 def draw_game_state(screen, currState, validMoves, squareSelected):
     draw_board(screen)
     highlight_squares(screen, currState, validMoves, squareSelected)
@@ -41,10 +62,9 @@ def draw_game_state(screen, currState, validMoves, squareSelected):
 
 
 def draw_board(screen):
-    colors = [p.Color("white"), p.Color("gray")]
     for r in range(DIMS):
         for c in range(DIMS):
-            color = colors[(r + c) % 2]
+            color = p.Color(COLORS[(r + c) % 2])
             p.draw.rect(screen, color, p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
@@ -64,6 +84,7 @@ def main():
     currState = ChessEngine.GameState()
     validMoves = currState.get_all_valid_moves()
     moveMade = False
+    animate = False
     load_images()
     running = True
     squareSelected = ()
@@ -88,6 +109,7 @@ def main():
                         for i in range(len(validMoves)):
                             if move == validMoves[i]:
                                 moveMade = True
+                                animate = True
                                 currState.make_move(validMoves[i])
                                 # reset
                                 squareSelected = ()
@@ -100,8 +122,11 @@ def main():
                 if e.key == p.K_z:  # if 'z' is pressed
                     currState.undo_move()
                     moveMade = True
+                    animate = False
 
         if moveMade:
+            if animate:
+                animate_move(currState.moveLog[-1], screen, currState.board, clock)
             validMoves = currState.get_all_valid_moves()
             moveMade = False
 
